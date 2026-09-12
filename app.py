d5ee5a5adf8c0756bcacd3356c7c8645fa971820
ayomide891 <macaulyayomide2566@gmail.com>
2026-09-12 02:40:27 +0100

    Remove info message about customer care
    Removed unnecessary info message from the app.

--- app.py
+++ app.py
@@ -30,5 +30,3 @@ if st.button("🚨 ANALYZE & RECOMMEND", type="primary"):
     st.metric("Most Important Machine to Fix", Most_important)
     st.metric("Highest Downtime", f"{Highest} mins")
     st.write(f"**Manager:** {Manager_Name} | **Branch:** {Branch_Office}")
-    st.info("--WE CARE FOR YOUR 
-
